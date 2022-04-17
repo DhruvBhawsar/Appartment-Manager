687b288f15248160bc1863cc8a34c1219aec7622
Dhruv <bhawsardrv@gmail.com>
2022-04-17 16:22:19 +0530

    api

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const connect = require("./configs/db");
-
+const port = process.env.PORT || 8080;
 // const flatController = require("./controllers/flat.controller");
 
 const app = express();
 app.use(express.json());
 // app.use("/flat",flatController)
 
-app.listen(2323, async () => {
+app.listen(port, async () => {
   try {
     await connect();
-    console.log("listening on 2323");
+    console.log(`listening on ${port}`);
   } catch (err) {
     console.log(err.message);
   }
