20c9612ffd25332c30de05263ed422695e20864e
Dhruv <bhawsardrv@gmail.com>
2022-04-17 23:15:03 +0530

    some changes

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-  return mongoose.connect(process.env.URL);
+  return mongoose.connect(
+    "mongodb+srv://dhruv:drv123@cluster0.on2yu.mongodb.net/appartments?retryWrites=true&w=majority"
+  );
 };
